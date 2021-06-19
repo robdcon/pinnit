1560f17e0cce096a9c0d1d105540b47119a46c2b
robdcon <rob@innerspace.digital>
2021-06-19 14:02:06 +0100

    update basic server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
-  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
