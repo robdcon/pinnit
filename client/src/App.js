64eda76e8b73b0e14d076b3e66572fae52a7fdf1
robdcon <rob@innerspace.digital>
2021-06-19 20:44:37 +0100

    installs backend to build and serve react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Note from "./components/Note"
 import Board from "./components/Board"
 
+const App = () => {
+  const [data, setData] = useState({notes:[{message:'note one'}]});
 
-class App extends Component 
-{
-  render() 
-  {
-    return (
-      <div className="Pinnit">
-        <Board />
-      </div>
-    );
-  }
+  useEffect(() => {
+    fetch('/api')
+    .then((res) => res.json())
+    .then((data) => setData(data));
+  }, []);
+
+  return(
+    <div className="Pinnit">
+      <Board />
+    </div>
+  )
 }
 
 export default App;
