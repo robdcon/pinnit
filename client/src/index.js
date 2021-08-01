d015853c66f4ee7b212c0d720d99d11db6acc24f
robdcon <rob@innerspace.digital>
2021-08-01 14:50:48 +0100

    use apollo provider for app

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './registerServiceWorker';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphiql'
+})
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
