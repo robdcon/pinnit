3e6e2c79c23710f3c0efb19cc8f745e9b39b5cd4
Rob Connolly <rconnolly@macmillan.org.uk>
2024-10-01 12:31:55 +0100

    remove dotenv ref

--- server/datasources/database.js
+++ server/datasources/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_APIKEY);
 
