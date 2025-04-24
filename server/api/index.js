6ddf5089230a1cfb8adc5e6b2273e1002cee8f31
robdcon <robdcon@gmail.com>
2025-04-24 23:06:23 +0100

    test db connection

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,27 @@
-// require('dotenv').config()
+require('dotenv').config()
+
 const cors = require('cors')
 const express = require("express");
 const { ApolloServer } = require('apollo-server-express');
 const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const client = require('../datasources/database');
 const http = require('http');
+
+const { Pool, Client } = require('pg')
+const connectionString = "postgresql://ujh2dxkwe2aar:fcqqhna5ioz3@robdcon.co.uk:5432/dbkvhbnfgnd7qh"
+
+const pgPool = new Pool({
+  connectionString
+});
+
+const test = async () => {
+  const client = await pgPool.connect();
+  const res = await client.query('SELECT NOW()')
+  console.log('Server time:', res.rows[0].now);
+  client.release()
+}
+
+test().catch(e => console.error(e.stack))
 
 const resolvers = require('../resolvers');
 const typeDefs = require('../schema');
@@ -19,7 +36,7 @@ const corsOptions = {
 };
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send(`Hey this is my API running 🥳 ${process.env.TEST_TEXT}`)
@@ -37,8 +54,15 @@ async function startApolloServer(app, httpServer) {
   });
 
   await server.start();
-
+  
   server.applyMiddleware({ app, path:'/graphiql' });
+  
+  await new Promise((resolve) =>
+    httpServer.listen({ port: PORT }, resolve),
+  ).then(() => {
+    console.log('USER:', process.env.PGUSER)
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 }
 
 startApolloServer(app, httpServer);
