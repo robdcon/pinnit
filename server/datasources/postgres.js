0b1aaa21c06654d4514bfa463693fb0257a25e3e
robdcon <robdcon@gmail.com>
2025-05-02 12:38:11 +0100

    add properties to pool

--- server/datasources/postgres.js
+++ server/datasources/postgres.js
@@ -1,7 +1,24 @@
+require('dotenv').config();
 const pg = require('pg');
 const { Pool } = pg
-const cs = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
+// const connectionString = "host=35.214.59.104 port=5432 dbname=dbkvhbnfgnd7qh user=uxcbxdwm5ywui password=xxxxxxx connect_timeout=10 sslmode=prefer";
+const connectionString = process.env.PG_CONNECTION_STRING
+const pgPool = new Pool({
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+});
 
-const pgPool = new Pool()
 
-module.exports = pgPool
+const pgClient = new Pool({
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+})
+
+module.exports.pgPool = pgPool
+module.exports.pgClient = pgClient
